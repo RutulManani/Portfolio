ef59d7c99cf5b19f6315ac3eb50e97f9ac023960
Rutul Manani <rutulmanani18@gmail.com>
2025-07-16 22:05:50 -0400

    added file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         <div className="about-content">
           <div className="about-text">
             <p>
-              I'm a Toronto-based UX/UI designer and web developer with 3 years of cross-industry experience and a background in Industrial Design. Currently pursuing post-graduate studies in UX Design and Web Development at Humber College, I bring a blend of design thinking and technical expertise to craft intuitive digital experiences. From insurance to AI-driven platforms, I bridge design and development to deliver human-centered, scalable solutions.
+              I'm a Toronto-based UX-UI designer and web developer with 3 years of cross-industry experience and a background in Industrial Design. Currently pursuing post-graduate studies in UX Design and Web Development at Humber College, I bring a blend of design thinking and technical expertise to craft intuitive digital experiences. From insurance to AI-driven platforms, I bridge design and development to deliver human-centered, scalable solutions.
             </p>
             <div className="skills">
               {skills.map((skill, index) => (
